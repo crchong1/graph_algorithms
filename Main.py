1d493fd785a6f9f3fa8b1381b491c62deeb02c48
Connor Chong <chong.connor@gmail.com>
2019-01-31 16:25:35 -0500

    finished topo sort

--- Main.py
+++ Main.py
@@ -1,11 +1,15 @@
 from Graph import DirectedGraph, UndirectedGraph, Node
-
+from TopoSort import TopoSort
 def main():
-    g = UndirectedGraph()
+    g = DirectedGraph()
     g.updateNeighbor(1,2)
     g.updateNeighbor(1,3)
-    g.updateNeighbor(2,3)
-    print(g.getGraph())
+    g.updateNeighbor(1,4)
+    g.updateNeighbor(2,5)
+    g.updateNeighbor(2,6)
+
+    t = TopoSort(g.getGraph())
+    print(t.topologicalSort())
 
 if __name__ == "__main__":
     main()
--- TopoSort.py
+++ TopoSort.py
@@ -1,10 +1,29 @@
 class TopoSort:
     def __init__(self, graph):
         self.graph = graph
-        self.stack = []
+        self.topologicalOrdering = []
         self.visited = set()
 
-    def TopoSort(self):
+    def topologicalSort(self):
+        for key in self.graph:
+            if(key not in self.visited):
+                self.topologicalSortFromNode(key)
+        self.topologicalOrdering.reverse()
+        return self.topologicalOrdering
 
-    def TopoSortFromNode(self, nodeID):
+
+    def topoRecursiveHelper(self, nodeID):
+        neighbors = self.graph[nodeID].getNeighbors()
+        for n in neighbors:
+            if n not in self.visited:
+                self.visited.add(n)
+                self.topoRecursiveHelper(n)
+        self.topologicalOrdering.append(nodeID)
+
+    def topologicalSortFromNode(self, startID):
+        self.visited.add(startID)
+        self.topoRecursiveHelper(startID)
+        
+        
+
         
